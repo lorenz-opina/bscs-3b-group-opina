a777f774bf892a0a37337d9fca2b0a2385af6884
Aleph-04 <lorenzopina22@gmail.com>
2025-06-15 12:39:08 +0800

    vt

---
+++ src/main/java/com/example/bscs_3b_group_opina/Bscs3bGroupOpinaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bscs_3b_group_opina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bscs3bGroupOpinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bscs3bGroupOpinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bscs_3b_group_opina/Bscs3bGroupOpinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bscs_3b_group_opina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bscs3bGroupOpinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
