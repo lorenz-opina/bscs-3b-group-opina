73d011babbed74d895d70dadf2d3d0b43b6ddf1b
Aleph-04 <lorenzopina22@gmail.com>
2025-06-17 19:27:02 +0800

    client stuff
    [added new project for client]

---
+++ bscs-3b-opina-client/src/test/java/com/example/bscs_3b_opina_client/Bscs3bOpinaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bscs_3b_opina_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bscs3bOpinaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
